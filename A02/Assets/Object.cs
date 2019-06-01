e0e03612798cd309918a0dec82223c2a615e50d6
Gia Nguyen <nhgia@apcs.vn>
2019-06-02 04:10:12 +0700

    A02
    A02 submitted

---
+++ A02/Assets/Object.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    public GameObject[] gemObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomM = 5;
+        int randomN = 5;
+        for (int i = 0; i < randomM; i++)
+            for (int j = 0; j < randomN; j++)
+            {
+                int rndDiamond = Random.Range(0, gemObjects.Length);
+                Instantiate(gemObjects[rndDiamond], new Vector2(i * 1.5f,j * 1.5f), Quaternion.identity);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
